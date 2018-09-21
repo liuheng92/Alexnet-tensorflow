1cc676c01b366c8492f4cdbe648077171714ec6a
liuheng <1041017843@qq.com>
2018-09-21 17:42:24 +0800

    Alexnet for kaggle(dog vs cat)

---
+++ readdata.py
@@ -0,0 +1,92 @@
+import tool_util
+from data_util import GeneratorEnqueuer
+import numpy as np
+import os, glob, cv2
+import tensorflow as tf
+import time
+
+logger = tool_util.logger
+
+tf.app.flags.DEFINE_string('training_image_path', './kaggle/train/',
+                           'training images to use')
+tf.app.flags.DEFINE_string('training_label_file', './VOCdevkit/VOC2007/ImageSets/labels.txt',
+                           'training labels to use')
+FLAGS = tf.app.flags.FLAGS
+
+
+def get_images():
+    filelist = []
+    for ext in ['jpg', 'JPG', 'jpeg', 'png']:
+        filelist.extend(glob.glob(os.path.join(FLAGS.training_image_path, '*.{}'.format(ext))))
+    return filelist
+
+def get_lables():
+    labellist = {}
+    with open(FLAGS.training_label_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            key,value = line.split(' ')
+            labellist[key] = int(value)
+
+def onehot(index):
+	""" It creates a one-hot vector with a 1.0 in
+		position represented by index
+	"""
+	onehot = np.zeros(2)
+	onehot[index] = 1.0
+	return onehot
+
+def generator(input_size=224, batch_size=32):
+    imagelist = np.array(get_images())
+    index = np.arange(0, imagelist.shape[0])
+    while True:
+        np.random.shuffle(index)
+        images = []
+        lables = []
+        for i in index:
+            try:
+               image = cv2.imread(imagelist[i])
+               h,w,_=image.shape
+               assert h>0 and w>0
+               image = cv2.resize(image, (input_size, input_size))
+               images.append(image[:,:,::-1].astype(np.float32))
+               #0 -- cat, 1 -- dog
+               if os.path.basename(imagelist[i]).split('.')[0]=='cat':
+                   lables.append(onehot(0))
+               elif os.path.basename(imagelist[i]).split('.')[0]=='dog':
+                   lables.append(onehot(1))
+               else:
+                   logger.error(imagelist[i]+' wrong name')
+                   continue
+
+               if len(images) == batch_size:
+                   yield images, lables
+                   images = []
+                   lables = []
+            except Exception as e:
+                import traceback
+                traceback.print_exc()
+                continue
+
+
+def get_batch(num_workers, **kwargs):
+    try:
+        enqueuer = GeneratorEnqueuer(generator(**kwargs), use_multiprocessing=True)
+        enqueuer.start(max_queue_size=24, workers=num_workers)
+        generator_output = None
+        while True:
+            while enqueuer.is_running():
+                if not enqueuer.queue.empty():
+                    generator_output = enqueuer.queue.get()
+                    break
+                else:
+                    time.sleep(0.01)
+            yield generator_output
+            generator_output = None
+    finally:
+        if enqueuer is not None:
+            enqueuer.stop()
+
+
+if __name__=='__main__':
+    generator()
