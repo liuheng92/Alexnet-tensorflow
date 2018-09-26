3a14acb5c129292d38c850cbeb3864e75693769f
liuheng <1041017843@qq.com>
2018-09-26 13:28:07 +0800

    fix test.py bug

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import time
 
 #only support 224 in this alexnet
 tf.app.flags.DEFINE_integer('input_size', 224, '')
-tf.app.flags.DEFINE_string('test_image_path', './kaggle/test/','test images to use')
+tf.app.flags.DEFINE_string('test_image_path', './kaggle/test1/','test images to use')
 tf.app.flags.DEFINE_string('gpu_list', '', '')
 tf.app.flags.DEFINE_string('checkpoint_path', './classify/', '')
 tf.app.flags.DEFINE_integer('num_classes', 2, '')
@@ -26,7 +26,12 @@ def main(argv=None):
 
         input_images = readdata.mean_image_subtraction(input_images)
         with slim.arg_scope(alexnet.alexnet_v2_arg_scope()):
-            outputs, _ = alexnet.alexnet_v2(input_images, num_classes=FLAGS.num_classes, )
+            outputs, _ = alexnet.alexnet_v2(input_images, num_classes=FLAGS.num_classes, is_training=False)
+            logger.debug(outputs.get_shape().as_list())
+            squeeze_outputs = tf.squeeze(outputs)
+            logger.debug(tf.shape(squeeze_outputs))
+            probs = tf.nn.softmax(squeeze_outputs)
+            logger.debug(tf.shape(probs))
 
             saver = tf.train.Saver()
             with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
@@ -36,23 +41,23 @@ def main(argv=None):
                 saver.restore(sess, ckpt_state.model_checkpoint_path)
 
                 image_list = readdata.get_images(FLAGS.test_image_path)
-                for image_name in image_list:
-                    im = cv2.imread(image_name)[:,:,::-1]
 
-                    img = im.copy()
+                import csv
+                with open('results.csv', 'w') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(["id","label"])
+                    for image_name in image_list:
+                        logger.info(image_name)
+                        im = cv2.imread(image_name)[:,:,::-1]
 
-                    im_resized=cv2.resize(img, (FLAGS.input_size, FLAGS.input_size))
-                    start_time = time.time()
-                    logger.info(image_name)
-                    outputs = sess.run([outputs], feed_dict={input_images:[im_resized], keep_prob:1.0})
+                        img = im.copy()
 
-                    input_shape = tf.shape(outputs)
-                    outputs_shaped = tf.reshape(outputs, [-1, input_shape[-1]])
-                    logger.debug(outputs_shaped)
-                    prob = tf.nn.softmax(outputs_shaped[-1])
-                    logger.debug(prob)
-                    # logger.info('detection time:{:.0f}ms result:(cat:{:.3f} dog:{:.3f})'.format(
-                    #     (time.time()-start_time)*1000, outputs[0][0][0], outputs[0][0][1]))
+                        im_resized=cv2.resize(img, (FLAGS.input_size, FLAGS.input_size))
+                        start_time = time.time()
+                        prob = sess.run([probs], feed_dict={input_images:[im_resized], keep_prob:1.0})
+                        writer.writerow([os.path.basename(image_name).split('.')[0], prob[0][1]])
+                        logger.info('detection time:{:.0f}ms result:(cat:{:.3f} dog:{:.3f})'.format(
+                            (time.time()-start_time)*1000, prob[0][0], prob[0][1]))
 
 if __name__=='__main__':
     tf.app.run()
