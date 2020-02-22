240e0c695a6109350ee17b74d19d93babe6a969b
Michael liu <1041017843@qq.com>
2020-02-22 22:35:37 +0800

    add preprocessing

--- readdata.py
+++ readdata.py
@@ -4,6 +4,7 @@ import numpy as np
 import os, glob, cv2
 import tensorflow as tf
 import time
+import random
 
 logger = tool_util.logger
 
@@ -44,11 +45,54 @@ def get_lables():
 
 def onehot(index):
     """ It creates a one-hot vector with a 1.0 in
-		position represented by index
-	"""
-    onehot = np.zeros(2)
+            position represented by index
+    """
+    onehot = np.zeros(3)
     onehot[index] = 1.0
     return onehot
+
+def random_rgb2gray(img, p=0.08):
+
+    if random.random() < p:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    return img
+
+def random_horizontal_flip(img, p=0.5):
+
+    if random.random() < p:
+        _, w_img, _ = img.shape
+        img = img[:, ::-1, :]
+
+    return img
+
+def random_exposure(img, delta=20, p=0.5):
+    if random.random() < p:
+        exposure = np.random.uniform(-delta, delta)
+        img_type = img.dtype
+        img = img.astype(np.float32)
+        img += exposure
+        img = np.clip(img, 0, 255)
+        img = img.astype(img_type)
+    return img
+
+#todo:默认是0-255图像
+def random_hsv(img, hue = 2, saturation=5, value=0, p=0.5):
+    if random.random() < p:
+        h = np.random.uniform(-hue, hue)
+        s = np.random.uniform(-saturation, saturation)
+        # v = np.random.uniform(-value, value)
+        img_hsv= cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        img_type = img.dtype
+        img_hsv = img_hsv.astype(np.float32)
+        #default img 0-255, hue 0-180
+        img_hsv[..., 0] = np.clip(img_hsv[..., 0]+h, 0, 180)
+        img_hsv[..., 1] = np.clip(img_hsv[..., 1]+s, 0, 255)
+        # img_hsv[..., 2] = np.clip(img_hsv[..., 2]+v, 0, 255)
+        img_hsv = img_hsv.astype(img_type)
+        img = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2RGB)
+    return img
 
 def generator(input_size=224, batch_size=32):
     imagelist = np.array(get_images())
@@ -60,6 +104,12 @@ def generator(input_size=224, batch_size=32):
         for i in index:
             try:
                image = cv2.imread(imagelist[i])
+               if image is None:
+                   continue
+               image = random_horizontal_flip(image)
+               image = random_hsv(image)
+               image = random_exposure(image)
+               image = random_rgb2gray(image)
                h,w,_=image.shape
                assert h>0 and w>0
                image = cv2.resize(image, (input_size, input_size))
